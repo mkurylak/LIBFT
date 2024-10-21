ab7d303907874eb2f3fb09008e9b2a8c9b38f85f
makuryla@student.42angouleme.fr <makuryla@1A7.42angouleme.fr>
2024-10-21 13:06:03 +0200

    fly

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,9 +6,51 @@
 /*   By: makuryla <makuryla@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/19 15:42:38 by makuryla          #+#    #+#             */
-/*   Updated: 2024/10/19 16:21:42 by makuryla         ###   ########.fr       */
+/*   Updated: 2024/10/21 13:05:11 by makuryla         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+int	ft_getlen(int nb)
+{
+	int	len;
+
+	len = 0;
+	if (nb < 1)
+		len++;
+	while (nb)
+	{
+		nb /= 10;
+		len++;
+	}
+	return (len);
+}
+
+char	*ft_itoa(int n)
+{
+	int				len;
+	unsigned int	nb;
+	char			*str;
+
+	len = ft_getlen(n);
+	nb = n;
+	str = malloc(sizeof(char) * (len + 1));
+	if (!str)
+		return (NULL);
+	if (n < 0)
+	{
+		str[0] = '-';
+		nb = -nb;
+	}
+	else if (n == 0)
+		str[0] = '0';
+	str[len--] = '\0';
+	while (nb)
+	{
+		str[len] = nb % 10 + '0';
+		len--;
+		nb = nb / 10;
+	}
+	return (str);
+}
